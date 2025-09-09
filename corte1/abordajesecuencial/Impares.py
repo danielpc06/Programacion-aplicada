9a33639917ed974f802b5b3ccd8194ee0752e101
danielpc06 <dapenalozac@udistrital.edu.co>
2025-09-09 13:08:43 -0500

    Add files via upload

---
+++ corte1/abordajesecuencial/Impares.py
@@ -0,0 +1,11 @@
+#Este ejercicio vale 1 decima para el corte 1 
+
+while True:
+    num=input("ingrese un numero: ")
+    num=int(num)
+    residual=num%2
+    if residual==0:
+        print("el numero ingresado es par ")
+    else:
+        print("el numero ingresado es impar ")
+
